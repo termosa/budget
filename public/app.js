285bb78c158c1c9388c982c1a4ccf7bac7112ac3
Stanislav Termosa <termosa.stanislav@gmail.com>
2016-05-17 01:20:03 +0300

    Create 'addTransaction' method
    Create Server module and implement 'addTransaction' method.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,3 @@
 (function(scope) {
   "use strict";
-
-  var settings = scope.settings;
-  var ref = new Firebase(settings.firebaseHost);
 })(window);
---
+++ public/server.js
@@ -0,0 +1,27 @@
+(function(scope) {
+  "use strict";
+
+  var settings = scope.settings;
+  var reference = new Firebase(settings.firebaseHost);
+  var transactions = reference.child('transactions');
+
+  function addTransaction(transaction) {
+    var time = transaction.time || Date.now();
+    var priority = 0 - time;
+    return transactions.push()
+      .setWithPriority({
+        title: transaction.title || null,
+        time: time,
+        category: transaction.category || null,
+        amount: transaction.amount || null,
+        currency: transaction.currency || null,
+        description: transaction.description || null,
+        location: transaction.location || null
+      }, priority);
+  }
+
+  scope.server = {
+    reference: reference,
+    addTransaction: addTransaction
+  };
+})(window);
